351b0a849f6b1d2a1f0e5a8010d69e46163e76fb
SeePeaJay <seercookie@gmail.com>
2021-12-31 17:06:33 -0800

    Remove padding and text-align properties
    This change properly displays list items and other kinds of block when their content needs to wrap

--- src/App.vue
+++ src/App.vue
@@ -17,14 +17,13 @@ export default {
 <style>
 * { /* perhaps there's a better way of resetting? */
 	margin: 0;
-	padding: 0;
+	/* padding: 0; */
 }
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 	display: flex;
 	flex-direction: column;
