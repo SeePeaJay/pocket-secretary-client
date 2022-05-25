599c962a025e409f08befd3ce4e80118e979ce82
SeePeaJay <seercookie@gmail.com>
2022-05-25 04:06:34 -0700

    feat~: links that lead to nonexistent engram now create it

---
+++ src/components/EngramLink.vue
@@ -0,0 +1,23 @@
+<template>
+	<router-link
+		v-if="engramDoesExist"
+		:to="{ name: 'Engram', params: { engramTitle: getEngramTitle(chunk) } }"
+	>
+	</router-link>
+	<span v-else class="placeholder" @click="createAndGoToEngram">
+	</span>
+</template>
+
+<script>
+export default {
+	name: 'EngramLink',
+	props: {
+  },
+};
+</script>
+
+<style scoped>
+/* want empty link appearance */
+.placeholder {
+}
+</style>
