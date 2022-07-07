e8a72cab37ba7eb753348abba990309bf209dc75
SeePeaJay <seercookie@gmail.com>
2022-07-07 01:46:53 -0700

    fix: new engram from links are now sorted alphabetically

--- src/views/Engrams.vue
+++ src/views/Engrams.vue
@@ -19,7 +19,20 @@ export default {
   name: 'Engrams',
   computed: {
 		engrams() {
-			return this.$store.state.engrams;
+			return [...this.$store.state.engrams].sort((a, b) => {
+				const titleA = a.title.toUpperCase();
+				const titleB = b.title.toUpperCase();
+
+				if (titleA < titleB) {
+					return -1;
+				}
+
+				if (titleA > titleB) {
+					return 1;
+				}
+
+				return 0;
+			});
 		},
 	},
 	methods: {
